b326d7349c3074c5ac74c91de839fd1ee0bd7d95
romainDonne22 <rom.donne@gmail.com>
2024-10-22 11:18:47 +0200

    first commit

---
+++ dossierAdeployer/script.py
@@ -0,0 +1,257 @@
+import socket
+import json
+import struct
+import threading
+import os
+import time
+
+# Obtenir le nom de la machine
+nom_machine = socket.gethostname()
+PORT = 4444
+PORT2 = 4445
+print(f"'{nom_machine}' : Bonjour, je suis la machine ")
+
+messagePostSuffle=[] #variable pour stocker les messages après le suffle
+
+# Créer un socket TCP/IP
+serveur_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Lier le socket à l'adresse et au port avec un maximum de 5 tentatives
+for tentative in range(5):
+    try:
+        serveur_socket.bind(('0.0.0.0', PORT))
+        print(f"'{nom_machine}' : Le socket est lié au port {PORT} après {tentative + 1} tentative(s).")
+        break
+    except OSError:
+        if tentative < 4:
+            # Si le port est déjà utilisé, libérer le port en utilisant la commande kill
+            print(f"'{nom_machine}' : Le port {PORT} est déjà utilisé. Tentative de libération du port ({tentative + 1}/5)...")
+            # Afficher avec print le PID du processus qui utilise le port
+            pid = os.popen(f'lsof -t -i:{PORT}').read().strip()
+            print(f"'{nom_machine}' : PID du processus qui utilise le port {PORT} : {pid}")
+            if pid:
+                # Libérer le port et afficher le résultat de kill
+                os.system(f'kill -9 {pid}')
+                print(f"'{nom_machine}' : Tentative de tuer le processus {pid}.")
+            else:
+                print(f"'{nom_machine}' : Aucun processus n'utilise le port {PORT}.")
+            time.sleep(5)
+        else:
+            raise Exception(f"'{nom_machine}' : Impossible de lier le socket au port {PORT} après 5 tentatives.")
+
+# Créer un socket TCP/IP
+serveur_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Lier le socket à l'adresse et au port avec un maximum de 5 tentatives
+for tentative in range(5):
+    try:
+        serveur_socket2.bind(('0.0.0.0', PORT2))
+        print(f"'{nom_machine}' : Le socket est lié au port {PORT2} après {tentative + 1} tentative(s).")
+        break
+    except OSError:
+        if tentative < 4:
+            # Si le port est déjà utilisé, libérer le port en utilisant la commande kill
+            print(f"'{nom_machine}' : Le port {PORT2} est déjà utilisé. Tentative de libération du port ({tentative + 1}/5)...")
+            # Afficher avec print le PID du processus qui utilise le port
+            pid = os.popen(f'lsof -t -i:{PORT2}').read().strip()
+            print(f"'{nom_machine}' : PID du processus qui utilise le port {PORT2} : {pid}")
+            if pid:
+                # Libérer le port et afficher le résultat de kill
+                os.system(f'kill -9 {pid}')
+                print(f"'{nom_machine}' : Tentative de tuer le processus {pid}.")
+            else:
+                print(f"'{nom_machine}' : Aucun processus n'utilise le port {PORT2}.")
+            time.sleep(5)
+        else:
+            raise Exception(f"'{nom_machine}' : Impossible de lier le socket au port {PORT2} après 5 tentatives.")
+
+
+# Écouter les connexions entrantes
+serveur_socket.listen(5)
+print(f"'{nom_machine}' : PHASE 1 Le serveur écoute sur le port {PORT}...")
+
+serveur_socket2.listen(5)
+print(f"'{nom_machine}' : PHASE 2 Le serveur écoute sur le port {PORT2}...")
+
+connexions = {}
+connexions_phase_2 = {}
+
+def recevoir_exactement(client_socket, n):
+    data = b''
+    while len(data) < n:
+        packet = client_socket.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+def recevoir_message(client_socket):
+    # Recevoir la taille du message
+    taille_message_bytes = recevoir_exactement(client_socket, 4)
+    if taille_message_bytes is None:
+        print("Connexion fermée lors de la réception de la taille du message.")
+        return None
+
+    taille_message = struct.unpack('!I', taille_message_bytes)[0]
+
+    # Recevoir le message en utilisant la taille
+    data = recevoir_exactement(client_socket, taille_message)
+    if data is None:
+        print("Connexion fermée lors de la réception du message.")
+        return None
+    return data.decode('utf-8')
+
+def envoyer_message(client_socket, message):
+    try:
+        # Encoder le message en UTF-8
+        message_bytes = message.encode('utf-8')
+        # Envoyer la taille du message d'abord
+        client_socket.sendall(len(message_bytes).to_bytes(4, byteorder='big'))
+        # Envoyer le message
+        client_socket.sendall(message_bytes)
+    except BrokenPipeError:
+        print("Erreur: Broken pipe. La connexion a été fermée par l'autre côté.")
+    except Exception as e:
+        print(f"Erreur lors de l'envoi du message: {e}")
+
+def envoyer_message_liste(client_socket, message_liste):
+    try:
+        # Convertir le dictionnaire en chaîne JSON
+        message_json = json.dumps(message_liste)
+        # Envoyer la chaîne JSON
+        envoyer_message(client_socket, message_json)
+    except Exception as e:
+        print(f"Erreur lors de l'envoi du message: {e}")
+
+def gerer_connexion(client_socket, adresse_client):
+    print(f"'{nom_machine}' : Connexion acceptée de {adresse_client}")
+
+    # Ajouter la connexion au dictionnaire
+    connexions[adresse_client] = client_socket
+
+    nb_message=0
+    mots=[]
+    machines_reçues=[]
+    etat=1
+
+    while etat!=3:
+        if etat==1 and nb_message==0:
+            message_reçu = recevoir_message(client_socket)
+            # MAP
+            # Recevoir la liste des machines
+            print(f"'PHASE 1 {nom_machine}' : Message reçu: {message_reçu}")
+            machines_reçues = json.loads(message_reçu)
+            nb_message+=1
+            
+        elif etat==1 and nb_message==1:
+            # Recevoir le fichier WET
+            message_reçu = recevoir_message(client_socket)
+            # Supprimer les guillemets de la chaîne de caractères
+            message_reçu = message_reçu.replace('"', '')
+            # Ouvrir le fichier WET
+            with open('/cal/commoncrawl/'+ message_reçu, 'r') as file:
+                contenuWET = file.read() # Lire le contenu du fichier WET et le stocker dans une variable sous forme de chaîne de caractères
+            motsWET = contenuWET.split() # Diviser le contenu du fichier WET en mots
+
+            while message_reçu != "FIN PHASE 1":
+                print(f"'PHASE 1 {nom_machine}' : Message reçu: {message_reçu}")
+                #mots.append(message_reçu)
+                nb_message+=1
+                message_reçu = recevoir_message(client_socket)
+
+        elif message_reçu == "FIN PHASE 1":
+            while message_reçu != "GO PHASE 2":
+                print(f"'PHASE 1 {nom_machine}' : Message reçu: {message_reçu}")
+                etat=2
+                thread_accepter_phase2 = threading.Thread(target=accepter_connexion_phase2)
+                thread_accepter_phase2.start()
+                envoyer_message(client_socket, "OK FIN PHASE 1")
+                # Créer les connexions à toutes les machines
+                for machine in machines_reçues:
+                    try:
+                        # Créer un socket TCP/IP
+                        client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        # Se connecter à la machine
+                        client_socket2.connect((machine, PORT2))
+                        # Stocker la connexion
+                        connexions_phase_2[machine] = client_socket2
+                        print(f"'PHASE 2 {nom_machine}' : Connexion établie avec {machine}")
+                    except Exception as e:
+                        print(f"Erreur lors de la connexion à {machine}: {e}")
+                message_reçu = recevoir_message(client_socket)
+
+        elif message_reçu == "GO PHASE 2":
+            print(f"'PHASE 2 {nom_machine}' : Message reçu: {message_reçu}")
+            # SHUFFLE
+            for i, mot in enumerate(motsWET):
+                machine_number = len(mot)%len(machines_reçues)
+                #print(f"'PHASE 2 {nom_machine}' : Envoi de {mot} à {machines_reçues[machine_number]}")
+                envoyer_message(connexions_phase_2[machines_reçues[machine_number]], mot)
+                # Calculer et afficher le pourcentage de progression
+                pourcentage = (i + 1) / len(motsWET) * 100
+                if pourcentage % 5 == 0: # Afficher le pourcentage de progression toutes les 5% afin de ne pas surcharger la console
+                    print(f"'PHASE 2 {nom_machine}' : Progression du SHUFFLE: {pourcentage:.2f}%")
+
+            while message_reçu !="GO PHASE 3":    
+                envoyer_message(client_socket, "OK PHASE 2")
+                print(f"'PHASE 2 {nom_machine}' : Message envoyé: OK PHASE 2")
+                message_reçu = recevoir_message(client_socket)
+            
+        elif message_reçu == "GO PHASE 3": 
+            print(f"'PHASE 3 {nom_machine}' : Message reçu: {message_reçu}")
+            #print(f"'PHASE 3 {nom_machine}' : Message du Shuffle: {messagePostSuffle}")
+            compteur_mots = {}
+            # REDUCE
+            for mot in messagePostSuffle:
+                if mot in compteur_mots:
+                    compteur_mots[mot] += 1
+                else:
+                    compteur_mots[mot] = 1
+            envoyer_message(client_socket, "OK PHASE 3")
+            print(f"'PHASE 3 {nom_machine}' : Message envoyé: OK PHASE 3")
+            while message_reçu !="GO PHASE 4":
+                message_reçu = recevoir_message(client_socket)
+            
+        elif message_reçu == "GO PHASE 4":    
+            print(f"'PHASE 4 {nom_machine}' : Message reçu: {message_reçu}")
+            liste = [] # Créer une liste pour stocker les clés et les valeurs du dictionnaire car on ne peut pas envoyer un dictionnaire directement en TCP
+            for key, value in compteur_mots.items(): #le .items() permet de récupérer la clé et la valeur du dictionnaire
+                liste.append(key) # Ajouter la clé
+                liste.append(value) #Ajouter la valeur à la liste
+            #print(f"'PHASE 4 {nom_machine}' : Message envoyé post REDUCE: {liste}")
+            envoyer_message_liste(client_socket, liste) 
+            envoyer_message(client_socket, "OK PHASE 4")
+            print(f"'PHASE 4 {nom_machine}' : Message envoyé: OK PHASE 4")
+            etat=3 #pour quitter la boucle
+             
+
+def gerer_phase_2(client_socket, adresse_client):
+    print(f"'PHASE 2 {nom_machine}' : Gérer phase 2 pour {adresse_client}")
+    # Recevoir des messages spécifiques dans une boucle
+    while True:
+        message_reçu = recevoir_message(client_socket)
+        #print(f"'PHASE 2 {nom_machine}' : Message reçu: {message_reçu} de {adresse_client}")
+        messagePostSuffle.append(message_reçu)
+        
+
+def accepter_connexion_phase1():
+    # Accepter une nouvelle connexion
+    client_socket, adresse_client = serveur_socket.accept()
+    # Créer un thread pour gérer la connexion
+    thread_connexion = threading.Thread(target=gerer_connexion, args=(client_socket, adresse_client))
+    thread_connexion.start()
+
+def accepter_connexion_phase2():
+    while True:
+        # Accepter une nouvelle connexion
+        client_socket2, adresse_client = serveur_socket2.accept()
+        # Créer un thread pour gérer la connexion
+        thread_connexion = threading.Thread(target=gerer_phase_2, args=(client_socket2, adresse_client))
+        thread_connexion.start()
+
+# Créer et démarrer le thread pour accepter les connexions
+thread_accepter = threading.Thread(target=accepter_connexion_phase1)
+thread_accepter.start()
+
+# Attendre que les threads se terminent (ce qui n'arrivera probablement jamais)
+thread_accepter.join()
---
+++ envoyeur.py
@@ -0,0 +1,237 @@
+import socket
+import json
+import struct
+import threading
+
+# Lire les adresses des machines à partir du fichier machines.txt
+with open('machines.txt', 'r') as file:
+    machines = [line.strip() for line in file.readlines()]
+# Convertir la liste des machines en JSON
+machines_json = json.dumps(machines)
+
+# Lire le nom des fichiers WET à partir du fichier fichiersWET.txt
+with open('fichiersWET.txt', 'r') as file:
+    fichiersWET = [line.strip() for line in file.readlines()]
+
+# Tableaux pour stocker les états de fin de chaque phase
+tab_fin_phase_1 = [False]*len(machines)
+tab_fin_phase_2 = [False]*len(machines)
+tab_fin_phase_3 = [False]*len(machines)
+tab_fin_phase_4 = [False]*len(machines)
+
+# Les messages spécifiques à envoyer
+# messages_specifiques = ["un", "bonjour", "depuis", "la", "lune", "la", "lune", "est", "belle"]
+
+# Dictionnaire pour stocker les connexions
+connexions = {}
+
+# Créer les connexions à toutes les machines
+for machine in machines:
+    try:
+        # Créer un socket TCP/IP
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Se connecter à la machine
+        client_socket.connect((machine, 4444))
+        
+        # Stocker la connexion
+        connexions[machine] = client_socket
+        print(f"Connexion établie avec {machine}")
+    except Exception as e:
+        print(f"Erreur lors de la connexion à {machine}: {e}")
+
+def envoyer_message(client_socket, message):
+    # Convertir le message en bytes
+    message_bytes = message.encode('utf-8')
+    # Envoyer la taille du message en utilisant send
+    taille_message = struct.pack('!I', len(message_bytes))
+    total_envoye = 0
+    while total_envoye < len(taille_message):
+        envoye = client_socket.send(taille_message[total_envoye:])
+        if envoye == 0:
+            raise RuntimeError("La connexion a été fermée")
+        total_envoye += envoye
+    # Envoyer le message
+    client_socket.sendall(message_bytes)
+
+def envoyer_messages():
+    #Envoyer la liste des machines à chaque machine
+    for machine, client_socket in connexions.items():
+        try:
+            envoyer_message(client_socket, machines_json)
+            print(f"Envoyé la liste des machines à {machine}")
+        except Exception as e:
+            print(f"Erreur lors de l'envoi à {machine}: {e}")
+
+    # Envoyer les messages spécifiques de manière cyclique
+    # for index, message in enumerate(messages_specifiques):
+    #     machine_index = index % len(machines)
+    #     machine = machines[machine_index]
+    #     try:
+    #         client_socket = connexions[machine]
+    #         envoyer_message(client_socket, message)
+    #         print(f"Envoyé '{message}' à {machine}")
+    #     except Exception as e:
+    #         print(f"Erreur lors de l'envoi à {machine}: {e}")
+
+    # Algo de split X
+    # machine = machines[0]
+    # for index, message in enumerate(messages_specifiques):
+    #     if index == (0 or 1 or 2):
+    #         machine = machines[0]
+    #     elif index == (3 or 4):
+    #         machine = machines[1]
+    #     elif index == (5 or 6 or 7 or 8):
+    #         machine = machines[2]
+    #     try:
+    #         client_socket = connexions[machine]
+    #         envoyer_message(client_socket, message)
+    #         print(f"Envoyé '{message}' à {machine}")
+    #     except Exception as e:
+    #         print(f"Erreur lors de l'envoi à {machine}: {e}")
+
+    #Algo de split X pour envoyer les fichiers WET
+    machine = machines[0]
+    print(f"Envoi des noms des fichiers WET : ")
+    for index, fchWET in enumerate(fichiersWET):
+        if index == 0 :
+            machine = machines[0]
+        elif index == 1 :
+            machine = machines[1]
+        elif index == 2 :
+            machine = machines[2]
+        try:
+            fichierWET_json = json.dumps(fchWET)
+            client_socket = connexions[machine]
+            envoyer_message(client_socket, fichierWET_json)
+            print(f"Envoyé '{fchWET}' à {machine}")
+        except Exception as e:
+            print(f"Erreur lors de l'envoi à {machine}: {e}")
+
+    # Envoyer le message de fin de phase à chaque machine
+    for machine, client_socket in connexions.items():
+        try:
+            envoyer_message(client_socket, "FIN PHASE 1")
+            print(f"Envoyé 'FIN PHASE 1' à {machine}")
+        except Exception as e:
+            print(f"Erreur lors de l'envoi à {machine}: {e}")
+
+def recevoir_exactement(client_socket, n):
+    data = b''
+    while len(data) < n:
+        packet = client_socket.recv(n - len(data))
+        if not packet:
+            raise ConnectionError("Connexion fermée par le client")
+        data += packet
+    return data
+
+def recevoir_message(client_socket):
+    # Recevoir la taille du message
+    taille_message = struct.unpack('!I', recevoir_exactement(client_socket, 4))[0]
+    # Recevoir le message en utilisant la taille
+    data = recevoir_exactement(client_socket, taille_message)
+    return data.decode('utf-8')
+
+def recevoir_message_dict(client_socket):
+    try:
+        # Recevoir la taille du message (supposons qu'elle soit envoyée sur 4 octets)
+        taille_message = client_socket.recv(4)
+        if not taille_message:
+            raise ConnectionError("Connexion fermée lors de la réception de la taille du message.")
+        
+        taille_message = int.from_bytes(taille_message, byteorder='big')
+        
+        # Recevoir le message complet
+        data = recevoir_exactement(client_socket, taille_message)
+        if not data:
+            raise ConnectionError("Connexion fermée lors de la réception du message.")
+        
+        # Convertir la chaîne JSON en dictionnaire
+        message_dict = json.loads(data.decode('utf-8'))
+        
+        return message_dict
+    except Exception as e:
+        print(f"Erreur lors de la réception du message: {e}")
+        return None
+
+
+def recevoir_messages():
+    for machine, client_socket in connexions.items():
+        try:
+            message_reçu = recevoir_message(client_socket)
+            if message_reçu == "OK FIN PHASE 1":
+                print(f"Reçu '{message_reçu}' de {machine}")
+                tab_fin_phase_1[machines.index(machine)] = True
+                ######################## Phase 2 ########################
+                if all(tab_fin_phase_1):
+                    print("Toutes les machines ont fini la phase 1")
+                    print("---------------------------------------")
+                    for machine, client_socket in connexions.items():
+                        envoyer_message(client_socket, "GO PHASE 2")
+                        print(f"Envoyé 'GO PHASE 2' à {machine}")
+                    message_reçu = recevoir_message(client_socket)
+                    
+                    if message_reçu == "OK PHASE 2":
+                        for machine, client_socket in connexions.items():
+                            print(f"Reçu '{message_reçu}' de {machine}")
+                            tab_fin_phase_2[machines.index(machine)] = True
+                        ######################## Phase 3 ########################
+                        if all(tab_fin_phase_2):
+                            print("Toutes les machines ont fini la phase 2")
+                            print("---------------------------------------")
+                            lancer_phase_3()
+                            ######################## Phase 4 ########################
+                            if all(tab_fin_phase_3):
+                                print("Toutes les machines ont fini la phase 3")
+                                print("---------------------------------------")
+                                lancer_phase_4()
+                                if all(tab_fin_phase_4):
+                                    print("Toutes les machines ont fini la phase 4")
+                                    print("---------------------------------------")
+        except Exception as e:
+            print(f"Erreur lors de la réception de {machine}: {e}")
+
+
+def lancer_phase_3():
+    for machine, client_socket in connexions.items():
+        envoyer_message(client_socket, "GO PHASE 3")
+        print(f"Envoyé 'GO PHASE 3' à {machine}")
+        message_reçu = recevoir_message(client_socket)
+        if message_reçu == "OK PHASE 3":
+            for machine, client_socket in connexions.items():
+                print(f"Reçu '{message_reçu}' de {machine}")
+                tab_fin_phase_3[machines.index(machine)] = True
+                  
+def lancer_phase_4():
+    for machine, client_socket in connexions.items():
+        envoyer_message(client_socket, "GO PHASE 4")
+        print(f"Envoyé 'GO PHASE 4' à {machine}")
+    # Recevoir le REDUCE de chaque machine
+    for machine, client_socket in connexions.items():
+        message_recu = recevoir_message(client_socket)
+        while message_recu == "OK PHASE 3":
+            message_recu = recevoir_message(client_socket)
+        #print(f"Reçu '{message_recu}' de {machine}")
+        # Enregistrer la liste dans un fichier texte par machine
+        with open(f'output/resultats_phase_4_{machine}.txt', 'w') as fichier:
+            #for element in message_recu:
+                #fichier.write(f"{element}\n")
+            mots = message_recu.split("', '")   
+            fichier.write(f"{mots}\n")
+        print(f"Liste enregistrée dans 'resultats_phase_4_{machine}.txt'")
+    for machine, client_socket in connexions.items(): 
+        message_reçu = recevoir_message(client_socket)
+        if message_reçu == "OK PHASE 4":
+            print(f"Reçu '{message_reçu}' de {machine}")
+            tab_fin_phase_4[machines.index(machine)] = True
+
+# Créer et démarrer les threads pour envoyer et recevoir les messages
+thread_envoi = threading.Thread(target=envoyer_messages)
+thread_reception = threading.Thread(target=recevoir_messages)
+
+thread_envoi.start()
+thread_reception.start()
+
+# Attendre que les threads se terminent
+thread_envoi.join()
+thread_reception.join()
